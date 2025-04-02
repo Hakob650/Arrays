0b19b2ff2f0c5b4508089667c2ec4278d8c0a7dd
Hakob650 <hakob11meneshian@gmail.com>
2025-04-02 13:27:01 +0400

    Add files via upload

---
+++ arr9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int N=10;
+	int arr[N-1];
+	for(int i=0;i<N-1;++i){
+		scanf("%d",&arr[i]);
+	}for(int i=0;i<N;++i){
+		if(arr[i]==i+1){
+	}else{
+		printf("%d\n",i+1);
+		return 0;
+}
+}
+}
+
